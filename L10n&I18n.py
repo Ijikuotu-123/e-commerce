254fe86e9097de691950286ab525c212df1d0570
Ijikuotu-123 <anthonyijikuotu@gmail.com>
2023-06-29 18:27:06 +0100

    new commit

---
+++ L10n&I18n.py
@@ -0,0 +1,21 @@
+#LOCALIZATION
+# localization is what a product would look like if you were a native of the market
+# adapting features
+# modifying content
+#converting to local units and currencies 
+# using proper local format for dates, addresses, phone number e.tc
+
+#INTERNALIZATION
+# internationlization is the process of planning and implementing a product so it can be localized
+# e.g implementing a feature that will allow us add new lauguages to the product. Note the act of translating from 
+# from language to another isn't internalization but the features we have added that enable us do that.
+
+# TRANSLATION
+# Translation is the process of writing strings in one locale into another e.g British English to American English. And 
+# not from one language to another
+# English language can be American English, Canadian English e.t.c American English for example represent a locale
+# steps to achieve this:
+# 1. separate strings from your codes
+# 2. add an internalization layer to make adding new translations a breeze
+
+# note all our strings will be transported into a folder/directory called strings
